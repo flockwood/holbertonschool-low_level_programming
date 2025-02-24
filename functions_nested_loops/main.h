e641cc9e6b7b584955b2286373ddead04692e3c8
Holberton <flockwood@live.com>
2025-02-24 09:17:11 -0400

    fix main.h

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN
+#define MAIN
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -5,14 +8,10 @@ int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
-int print_last_digit(int);
+int print_last_digit(int n);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
+
+#endif
