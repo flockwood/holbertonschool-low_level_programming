c24a0445b517f070eb15b8dcd41c279b7004a721
Holberton <flockwood@live.com>
2025-02-24 11:11:03 -0400

    function checks for a digit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/** 
+ * _isdigit.c - checks for a digit (0 through 9) 
+ * @c: the character to check
+ *
+ * Return: 1 if c is uppercase, 0 otherwise.
+ */
+int _isupper(int c)
+{
+
+    if (c >= 0 && c <= 9)
+		return (1);
+	else
+		return (0);
+}
