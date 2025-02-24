ba49112578f642b9319b98c977d7696d70dce26e
Holberton <flockwood@live.com>
2025-02-24 11:19:52 -0400

    fix betty

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
-/** 
- * _isdigit.c - checks for a digit (0 through 9) 
+/**
+ * _isdigit - checks if a character is a digit.
  * @c: the character to check
  *
- * Return: 1 if c is uppercase, 0 otherwise.
+ * Return: 1 if c is a digit, 0 otherwise.
  */
 int _isdigit(int c)
 {
-
-	if (c >= '0' && c <= '9')
+	if (c >= '0' && c <= '9')  /* Check if c is between '0' and '9' */
 		return (1);
 	else
 		return (0);
 }
+
