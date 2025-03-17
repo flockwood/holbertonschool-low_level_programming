3dab48417798f2d09e7223396e400a35b4b9d00c
Holberton <flockwood@live.com>
2025-03-17 13:48:22 -0400

    fix

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -21,7 +21,7 @@ int int_index(int*array, int size, int (*cmp)(int))
 	int i;
 	if (array == NULL || cmp == NULL || size <= 0)
 		return (-1);
-	for (i = 0; 1 < size; i++)
+	for (i = 0; i < size; i++)
 	{
 	  if (cmp(array[i]) != 0)
 		  return (i);
