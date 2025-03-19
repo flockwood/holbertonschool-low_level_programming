5637546775ad0d424d481713a389cf7724480d5c
Holberton <flockwood@live.com>
2025-03-19 14:29:58 -0400

    fix betty

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -4,7 +4,7 @@
 
 /**
  * print_numbers - Prints numbers, followed by a new line
- * separator: the string to be printed between numbers
+ * @separator: the string to be printed between numbers
  * @n: the number of integers passed to the function
  *
  * Description: If separator is NULL, don't print it
