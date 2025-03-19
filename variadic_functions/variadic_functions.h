ff9140d889c6bc9164d2c1a45b238c8c43779487
Holberton <flockwood@live.com>
2025-03-19 11:20:32 -0400

    variadic functions

---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,10 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+#include <stdarg.h>
+#include <stdio.h>
+
+void print_all(const char * const format, ...);
+
+#endif /* VARIADIC_FUNCTIONS_H */
+
