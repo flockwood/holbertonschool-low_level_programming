5da6430e5d9256aad92cc296b8c8d8c5d66632b1
Holberton <flockwood@live.com>
2025-04-03 14:57:26 -0400

    fixed betty style

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -14,14 +14,14 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY |O_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
 
 	if (text_content != NULL)
 	{
 		while (text_content[len])
-				len++;
+			len++;
 		write_status = write(fd, text_content, len);
 		if (write_status == -1)
 		{
