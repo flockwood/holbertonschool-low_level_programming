c7ec58f47d768e02bf7db8cf12342774cd7e1266
Holberton <flockwood@live.com>
2025-04-03 12:52:39 -0400

    fixed some man issues

--- file_io/main.h
+++ file_io/main.h
@@ -17,14 +17,7 @@
  * @message: The error message to print.
  * @file_name: The file name to include in the error message.
  */
-void error_exit(int exit_code, const char *message, const char *file_name)
-{
-    if (file_name)
-        dprintf(STDERR_FILENO, "%s %s\n", message, file_name);
-    else
-        dprintf(STDERR_FILENO, "%s\n", message);
-    exit(exit_code);
-}
+
 
 int _putchar(int c);
 ssize_t read_textfile(const char *filename, size_t letters);
